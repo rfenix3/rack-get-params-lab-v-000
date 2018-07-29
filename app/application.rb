dcd212ab376036a2efeb158cbd8b34021cf990f7
Richard Fenix <richard.fenix@gmail.com>
2018-07-29 07:50:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,12 @@ class Application
       resp.write "Path Not Found"
     end
 
+    if req.path.match(/cart/)
+      @@cart.each do |cart_item|
+        resp.write "#{cart_item}\n"
+      end
+
+
     resp.finish
   end
 
